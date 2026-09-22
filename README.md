455de15894c77f405473547f5db011fa807a1d7d
Gowtham K R <gowthamkr008@gmail.com>
2026-09-22 15:13:24 +0000

    update project code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 git add .
 git commit -m "Updated code"
 git push
+# C_Program
--- projects/Steganography/C-Project-1/4-SkeletonCode/only_c_files/do_encoding.c
+++ projects/Steganography/C-Project-1/4-SkeletonCode/only_c_files/do_encoding.c
@@ -6,9 +6,5 @@ Status do_encoding(EncodeInfo *encInfo){
   /* copy first 54 byte*/
 
 
-  char header[54];
-  fread(header,53,1,encInfo->fptr_src_image);
-  fwrite(header,53,1,encInfo->fptr_stego_image);
-
   return e_success;
 }
